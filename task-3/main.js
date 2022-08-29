1b5f94b53b753e10978d023587a2e5286d531c5d
Ugur Rzayev <ughurrzayev@gmail.com>
2022-08-29 13:40:49 +0400

    Task 3 done

--- task-3/main.js
+++ task-3/main.js
@@ -4,6 +4,15 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    // Write code here
+    let a = result.input.split(' ')
+    let b = result.input
+    for (let i = 0; i < a.length; i++) {
+        if (a[i]>0) {
+            a[i]=parseInt(a[i])+parseInt(2)   
+        }
+        else if (a[i]<0) {
+            a[i]=parseInt(a[i])
+        }
+        } console.log(a);
   
 });
