10eaac159e5499714c375ba585b8e2a4840c2002
Ugur Rzayev <ughurrzayev@gmail.com>
2022-08-29 12:59:25 +0400

    Task 1 done

--- task-1/main.js
+++ task-1/main.js
@@ -4,6 +4,11 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    // Write code here
+    let array = result.input.split(" ")
+    let a = 0;
+    
+    for (let i = 0; i < array.length; i++) {
+        a = a + parseFloat(array[i])
+    } console.log(a);
   
 });
