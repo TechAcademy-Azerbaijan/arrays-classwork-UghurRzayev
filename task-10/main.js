d42eab2a1e9a6e6b9b2f5f46f9bbd3822bedd7c7
Ugur Rzayev <ughurrzayev@gmail.com>
2022-09-01 19:12:16 +0400

    Task 10 done

--- task-10/main.js
+++ task-10/main.js
@@ -4,6 +4,23 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    // Write code here
-  
+    let numbers = result.input.split(" ")
+    let max = parseInt(numbers[0])
+    let index = 0
+    for (let i = 0; i < numbers.length; i++) {
+        const element = numbers[i];
+        if (element>=max) {
+            max = element
+            index=i
+        }
+    } let a  = numbers.indexOf(numbers[index]);
+        numbers.splice(a, 1);
+        let numbers2 = numbers
+        let max2 = parseInt(numbers[0])
+        for (let j = 0; j < numbers.length; j++) {
+            const next_element = numbers[j];
+            if (next_element>=max2) {
+                max2 = next_element
+            }
+        } console.log(parseInt(max)+parseInt(max2));
 });
