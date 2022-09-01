5c86be5f7d14beb7f40118c1265bf16d8e81c65c
Ugur Rzayev <ughurrzayev@gmail.com>
2022-09-01 17:54:28 +0400

    Task 5 done

--- task-5/main.js
+++ task-5/main.js
@@ -4,6 +4,8 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    // Write code here
+    let a = result.input.split(" ")
+    a.unshift(a.pop())
+    console.log(a.join(" "));
   
 });
